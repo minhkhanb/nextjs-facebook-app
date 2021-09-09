97c26fd5b4782f577c1efe8ec42325f632f2c6c7
KhaLe <minhkhanb@gmail.com>
2021-09-09 21:02:56 +0700

    upload image

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,13 +1,46 @@
+import React from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/client';
 import { EmojiHappyIcon } from '@heroicons/react/outline';
 import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
+import { db } from '../firebase';
+import firebase from 'firebase';
 
 function InputBox() {
   const [session] = useSession();
+  const inputRef = React.useRef(null);
+  const filePickRef = React.useRef(null);
+  const [imageToPost, setImageToPost] = React.useState(null);
 
   const sendPost = (e) => {
     e.preventDefault();
+
+    if (!inputRef.current?.value) return;
+
+    db.collection('posts').add({
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    inputRef.current.value = '';
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -24,6 +57,7 @@ function InputBox() {
 
         <form className="flex flex-1">
           <input
+            ref={inputRef}
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
             type="text"
             placeholder={`What's on your mind, ${session.user.name}?`}
@@ -33,6 +67,15 @@ function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105"
+          >
+            <img className="h-10 object-contain" src={imageToPost} alt="" />
+          </div>
+        )}
       </div>
 
       <div className="flex justify-evenly p-3 border-t">
@@ -41,9 +84,10 @@ function InputBox() {
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
 
-        <div className="inputIcon">
+        <div onClick={() => filePickRef.current.click()} className="inputIcon">
           <CameraIcon className="h-7 text-green-400" />
           <p className="text-xs sm:text-sm">Photo/Video</p>
+          <input ref={filePickRef} onChange={addImageToPost} type="file" hidden />
         </div>
 
         <div className="inputIcon">

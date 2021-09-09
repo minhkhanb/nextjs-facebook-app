1add90efbd86940466efc8550c27fd9a6170047d
KhaLe <minhkhanb@gmail.com>
2021-09-09 20:20:18 +0700

    lint

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
-import firebase from "firebase";
-import "firebase/storage";
+import firebase from 'firebase';
+import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA92EiKUIsrM1zNiS6s4FS_GNaBr9X_5Rg",
-  authDomain: "facebook-2-0.firebaseapp.com",
-  projectId: "facebook-2-0",
-  storageBucket: "facebook-2-0.appspot.com",
-  messagingSenderId: "820973172142",
-  appId: "1:820973172142:web:afa7deeec79385cc6c443f",
+  apiKey: 'AIzaSyA92EiKUIsrM1zNiS6s4FS_GNaBr9X_5Rg',
+  authDomain: 'facebook-2-0.firebaseapp.com',
+  projectId: 'facebook-2-0',
+  storageBucket: 'facebook-2-0.appspot.com',
+  messagingSenderId: '820973172142',
+  appId: '1:820973172142:web:afa7deeec79385cc6c443f',
 };
 
 const app = !firebase.apps.length
